1592225c336e06ce5e52eeeb9ad5f29ff652b3fb
Maury Lindo <maury@cagedcrown.com>
2015-03-01 22:43:36 -0500

    added verbatim

--- js/_main.js
+++ js/_main.js
@@ -22,3 +22,12 @@ $(document).ready(function(){
 	// Target your .container, .wrapper, .post, etc.
 	$("#main").fitVids();
 });
+
+// Verbatim
+$(document).verbatim({
+ animated: true,
+ animationSpeed: 2000,
+ highlightParent: false,
+ highlightColor: 'rgb(134, 21, 210)',
+ bitlyToken: '417c7776b4faf0c1e927d1da2545a2e246402f94'
+});
